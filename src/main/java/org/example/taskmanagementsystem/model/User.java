fde4141a9131509442b979e8909a227d46687046
KaberaSamuel <kaberanshutis@gmail.com>
2026-01-13 23:42:44 +0200

    added user model

---
+++ src/main/java/org/example/taskmanagementsystem/model/User.java
@@ -0,0 +1,50 @@
+package org.example.taskmanagementsystem.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+    private LocalDate createdAt;
+
+    public User() {
+    }
+
+    public User(String username, String email, String password, String role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.createdAt = LocalDate.now();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+}
